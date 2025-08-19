b762fe1d1fd12248658247923d31709602d327c7
ray <120208761+theabsoluteray@users.noreply.github.com>
2025-08-19 23:42:05 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,9 +13,16 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      // Disable Next.js/React rules
       "@next/next/no-img-element": "off",
       "react/no-unescaped-entities": "off",
       "react-hooks/exhaustive-deps": "off",
+
+      // Disable TS linting rules
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // General JS rule disable
       "no-unused-vars": "off",
       "no-console": "off",
     },
@@ -23,3 +30,4 @@ const eslintConfig = [
 ];
 
 export default eslintConfig;
+
